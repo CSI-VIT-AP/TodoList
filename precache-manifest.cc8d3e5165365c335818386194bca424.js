0cad49a03711a28661ac49deae93b18aea4fd8c7
subhradwip01 <subhradwip01@gmail.com>
2022-03-27 00:17:24 +0530

    Updates

---
+++ precache-manifest.cc8d3e5165365c335818386194bca424.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af10c30c2948f593bc1a",
+    "url": "/TodoList/static/js/main.af10c30c.chunk.js"
+  },
+  {
+    "revision": "fc928b25625325103695",
+    "url": "/TodoList/static/css/1.2f00f733.chunk.css"
+  },
+  {
+    "revision": "fc928b25625325103695",
+    "url": "/TodoList/static/js/1.fc928b25.chunk.js"
+  },
+  {
+    "revision": "e5fd00049e7ac7cce407",
+    "url": "/TodoList/static/js/runtime~main.e5fd0004.js"
+  },
+  {
+    "revision": "5b625fcadb9f56677f44ba3e20126b9f",
+    "url": "/TodoList/static/media/CSILogo.5b625fca.png"
+  },
+  {
+    "revision": "6c4eb7526c0f9baac35c2795dc4b6e7e",
+    "url": "/TodoList/static/media/background.6c4eb752.png"
+  },
+  {
+    "revision": "4e7b150d2e0b7c1e2399f96d1291945c",
+    "url": "/TodoList/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.c206a5aa738307c1ec970d5754a460bc.js"
+  "/TodoList/precache-manifest.cc8d3e5165365c335818386194bca424.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/TodoList/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
